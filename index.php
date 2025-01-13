b9a4ee110f639b74969db4d5faff4a3590c224e8
felixvl <digitaleservice@gmail.com>
2025-01-13 11:40:06 +0100

    Naar Nieuwe pagina

--- index.php
+++ index.php
@@ -1,70 +1,15 @@
-ff kijken!!??
-
-
-<h1>hoi felix</h1>
-jouw saldo is: 700 euro
-<textarea></textarea>
-
-
-echo "felix";
-<br><br><br><br><br>
 <?php
+	$persoon = "Felix";
+	$wens = "goedemorgen";
+	$voornaam = "fred";
+	echo $wens." ".$voornaam;
 
-	echo "sandra";   // statement
-	// variabele
-		// waarde value
-	$x = 8;
-
-	echo "<br>";
-?>
-	<br>
-<?php
-	echo $x;
-	echo "<br>";
-	echo $x * 20;
-	//commentaar
-	// integer  double boolean "string"
-	$weekdag = "maandag";
-	echo $weekdag;
-	$keuze = true;
-	echo $keuze;
-	// operand
-		// operator
-			// operand
-	$getal   =      35.2;
-	echo $getal;
-	// operator
-	// assignment operator
-	// = += -= /= 
-	// arithmic operator
-	// + - * / %
-	// comparative operator --> evalueert tot boolean
-	// < >  ==  <=  >= !=
-	// logische operators --> operand dat die al een boolean 
-	// --> evalueert tot boolean
-	// && ||
-
-	echo $x < 5 || false;
-
-	echo "<br>";
-	echo 12 % 3;
-	$bedrag = 2295;
-	echo "<br>";
-	echo intval($bedrag / 100);
-	echo ".";
-	echo $bedrag % 100;
-	echo "<br>";
-	if( $x < 20){   // code conventie
-		echo "juist";
-	}else{
-		echo "onjuist";
-	}
-
-
-
-
-
-
-
+	echo 8 === "8";
+	echo "<h1> Hallo ".$persoon."</h1>";
+	//SELECT * FROM fiets WHERE merk = 'batavus';
+	$subcategorie = "fiets";
+	$merk = "batavus";
+	$sql = "SELECT * FROM ".$subcategorie." WHERE merk = '".$merk."';";
+	echo $sql;
 
 ?>
