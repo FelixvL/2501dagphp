e779bd62e7717c61c87d4d7d5ae0c8003371ce5b
felixvl <digitaleservice@gmail.com>
2025-01-13 15:13:45 +0100

    Naar Nieuwe pagina

--- vierdelesinvoerdata.php
+++ vierdelesinvoerdata.php
@@ -4,5 +4,7 @@
 ?>
 
 <form action="vierdelesuitvoerdata.php" method="GET">
+	<input type="color" name="ffkijken"><br>
+	<input type="file" name="watzouditzijn"><br>
 	<input type="submit" value="voer uit">
 </form>
--- vierdelesuitvoerdata.php
+++ vierdelesuitvoerdata.php
@@ -1,4 +1,19 @@
 <?php
 	echo "tweede stap";
+	echo $_GET["ffkijken"];
+	if(isset($_GET["ffkijken"])){
 
+	}
+
+?>
+
+<?php
+$conn = mysqli_connect("localhost", "root", "", "voorbeeld");
+
+$result = mysqli_query($conn, "SELECT * FROM gebruikers");
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "Naam: " . $row['naam'] . ", Email: " . $row['email'] . "<br>";
+}
+
+mysqli_close($conn);
 ?>
