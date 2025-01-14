d40c340bd6922f947cf41434b72d153f5161c808
felixvl <digitaleservice@gmail.com>
2025-01-14 11:45:31 +0100

    database met id

--- dag2_oop.php
+++ dag2_oop.php
@@ -4,18 +4,12 @@ hoi
 class Fiets{
     public $aantalbanden;
     public $kleur;
-
+    function remmen(){
+        echo "ik ga remmen";
+    }
 }
-
 // class Fiets{   // KAN NIET
-
 // }
-
-new Fiets();
-new Fiets();
-new Fiets();
-new Fiets();
-new Fiets();
 new Fiets();
 new Fiets();
 
@@ -23,5 +17,14 @@ new Fiets();
     $fiets1->kleur = "rood";
     $fiets1->aantalbanden = 4;
     echo $fiets1->kleur;
+    $qqlq = new Fiets();
+    $letsgo = new Fiets();
+    $letsgo->kleur = "blauw";  // foutmelding
+    echo $letsgo->kleur;
+    $fiets1->remmen();
+    $fiets1->remmen();
+    $fiets1->remmen();
+    $fiets1->remmen();
+    $fiets1->remmen();
 
 ?>
