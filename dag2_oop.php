5b5330f55af2e2486d46d2368dbacca997ef10fc
felixvl <digitaleservice@gmail.com>
2025-01-14 13:45:55 +0100

    database met id

--- dag2_oop.php
+++ dag2_oop.php
@@ -1,11 +1,49 @@
 hoi
 
 <?php
+
+
+function optellen(){
+    echo "8 plus 8 = 16";
+}
+function optellen2(){
+    echo "8 plus 9 = 17";
+}
+
+function optellenadvanced($getal){
+    echo "8 plus $getal = ".(8+$getal);
+}
+
+function echtoptellen($getal1, $getal2){
+    echo "$getal1 plus $getal2 = ".($getal1+$getal2);
+}
+optellen();
+echo "<hr>";
+optellen2();
+echo "<hr>";
+optellenadvanced(5);
+echo "<hr>";
+optellenadvanced(14);
+echo "<hr>";
+echtoptellen(45,55);
+echo "<hr>";
+echo "<hr>";
+
+
+
+
+
+
+
+
 class Fiets{
     public $aantalbanden;
     public $kleur;
     function remmen(){
-        echo "ik ga remmen";
+        echo "<br><br>ik ga remmen met de kleur ".$this->kleur;
+    }
+    function sturen($richting){
+        echo "<br><br>ik ga sturen in de richting: ".$richting;
     }
 }
 // class Fiets{   // KAN NIET
@@ -17,14 +55,23 @@ new Fiets();
     $fiets1->kleur = "rood";
     $fiets1->aantalbanden = 4;
     echo $fiets1->kleur;
+
+
+
+    echo "<hr>";
+    $fiets1->sturen("ZW");
+    echo "<hr>";
     $qqlq = new Fiets();
     $letsgo = new Fiets();
     $letsgo->kleur = "blauw";  // foutmelding
     echo $letsgo->kleur;
+    echo "<br>";
     $fiets1->remmen();
     $fiets1->remmen();
+    $letsgo->remmen();
     $fiets1->remmen();
-    $fiets1->remmen();
-    $fiets1->remmen();
+    echo $letsgo->remmen();
 
 ?>
+
+
