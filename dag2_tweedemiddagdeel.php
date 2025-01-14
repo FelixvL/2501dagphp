cb3891aa508eb66758cc8859cbeceec0f593d3b2
felixvl <digitaleservice@gmail.com>
2025-01-14 15:29:31 +0100

    database met id

--- dag2_tweedemiddagdeel.php
+++ dag2_tweedemiddagdeel.php
@@ -1,4 +1,9 @@
 <?php
+function functieaanroep(){
+
+    return "dit is mijn samengestelde zin";
+
+}
 echo $_SERVER["HTTP_HOST"];
 echo "<hr>";
 echo $_SERVER["PHP_SELF"];
@@ -10,8 +15,22 @@ echo "<hr>";
 //$go = shell_exec('dir');
 //echo $go;
 $file = fopen("bestand.txt", "w");
-fwrite($file, "Dit is een testbestand.");
+fwrite($file, functieaanroep());
 fclose($file);
+echo "<hr>";
+
+echo "Huidige directory: " . __FILE__;
+
+echo "<hr>";
+$filename = "example.txt";
+
+if (file_exists($filename)) {
+    unlink($filename);
+    echo "Bestand $filename is verwijderd.";
+} else {
+    echo "Bestand bestaat niet.";
+}
+mkdir("nieuwe_map");
 echo "<hr><hr><hr>";
 print_r($_SERVER);
 //var_dump($_SERVER);
