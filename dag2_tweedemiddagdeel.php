2759f896ba872371683dd2ccdfa165a2cdacf9a7
felixvl <digitaleservice@gmail.com>
2025-01-14 15:16:52 +0100

    database met id

--- dag2_tweedemiddagdeel.php
+++ dag2_tweedemiddagdeel.php
@@ -1,8 +1,17 @@
 <?php
 echo $_SERVER["HTTP_HOST"];
 echo "<hr>";
-echo $_SERVER["PATH"];
-
+echo $_SERVER["PHP_SELF"];
+echo "<hr>";
+print_r($GLOBALS);
+echo "<hr>";
+echo `dir ..`;
+echo "<hr>";
+//$go = shell_exec('dir');
+//echo $go;
+$file = fopen("bestand.txt", "w");
+fwrite($file, "Dit is een testbestand.");
+fclose($file);
 echo "<hr><hr><hr>";
 print_r($_SERVER);
 //var_dump($_SERVER);
