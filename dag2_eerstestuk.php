3798de89bbe71c8eec51fb40352d65d9e24da29f
felixvl <digitaleservice@gmail.com>
2025-01-14 10:35:13 +0100

    database met id

---
+++ dag2_eerstestuk.php
@@ -0,0 +1,33 @@
+<h1>dag 2</h1>
+<div>extra</div>
+tweede
+derde
+    vierde
+<?php
+/*
+    echo "<h1>zomaar</h1>\n";
+*/
+    # echo "<div>eronder</div>";
+    echo "\n\tvijfde ";
+    // echo "zesde";
+    $naam = "felix";
+    echo "allebei $naam";
+    echo 'mag $naam';
+    echo '<a href="link.php">go</a>';
+    echo "<a href=\"link.php\">go</a>";
+    define("VASTEWAARDE", 65);
+
+    echo "--".VASTEWAARDE;
+
+    $lijst = [45,3,2,66];
+
+    echo $lijst[1];
+    $_GET["id"]
+    $herinneringen = ["fiets.jpg","boot.jpg","bergen.jpg"];
+    echo $herinneringen[1];
+    echo "<img id=\"foto\" src=\"$herinneringen[1]\">";
+            // <img id="foto" src="boot.jpg">
+?>
+
+<!-- <img id="foto" src="boot.jpg"> -->
+<a href="link.php">go</a>
